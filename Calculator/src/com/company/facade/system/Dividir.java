584f4d44fea9c32692f87fd2782ac5ca4f14da26
JoseChavez98 <jose.chavez@utec.edu.pe>
2019-09-26 12:23:48 -0500

    adding functions

--- Calculator/src/com/company/facade/system/Dividir.java
+++ Calculator/src/com/company/facade/system/Dividir.java
@@ -1,4 +1,16 @@
 package com.company.facade.system;
 
 public class Dividir {
+
+    private float dividiendo;
+    private float divisor;
+
+    public Dividir(float dividiendo,float divisor){
+        this.dividiendo=dividiendo;
+        this.divisor=divisor;
+    }
+
+    public float exec(){
+        return dividiendo/divisor;
+    }
 }
--- Calculator/src/com/company/facade/system/Resta.java
+++ Calculator/src/com/company/facade/system/Resta.java
@@ -1,4 +1,17 @@
 package com.company.facade.system;
 
 public class Resta {
+    private float a;
+    private float b;
+
+    public Resta(float a, float b){
+        this.a=a;
+        this.b=b;
+
+    }
+
+    public float exec(){
+        return a-b;
+    }
+
 }
