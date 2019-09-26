e03e5729a53d9d884fcd8f0d82a98ca2a6aa84d2
Herless Alvarado <herlessalvarado@Herlesss-MacBook-Pro.local>
2019-09-26 12:21:16 -0500

    multiplicar y suma added

--- Calculator/src/com/company/facade/system/Multiplicar.java
+++ Calculator/src/com/company/facade/system/Multiplicar.java
@@ -1,4 +1,15 @@
 package com.company.facade.system;
 
 public class Multiplicar {
+    private float a;
+    private float b;
+
+    public Multiplicar(float a, float b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int exec(){
+        return a*b;
+    }
 }
--- Calculator/src/com/company/facade/system/Suma.java
+++ Calculator/src/com/company/facade/system/Suma.java
@@ -1,4 +1,15 @@
 package com.company.facade.system;
 
 public class Suma {
+    private float a;
+    private float b;
+
+    public Suma(float a, float b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int exec(){
+        return a+b;
+    }
 }
